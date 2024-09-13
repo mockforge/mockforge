c930a0a29ee346f69fb163e469694b8f5d090227
DiamondYuan <admin@diamondyuan.com>
2024-09-13 22:18:28 +0800

    fix: fix test case

--- src/extension/node/dirname.ts
+++ src/extension/node/dirname.ts
@@ -1,9 +1,10 @@
 export function getDirname() {
-  if (typeof __dirname !== 'undefined') {
-    return __dirname;
-  } else if (typeof import.meta !== 'undefined' && import.meta.url) {
+  if (typeof import.meta !== 'undefined' && import.meta.url) {
     // const __filename = fileURLToPath(import.meta.url);
     return new URL('.', import.meta.url).pathname;
+  }
+  if (typeof __dirname !== 'undefined') {
+    return __dirname;
   }
   throw new Error(`Unable to find current dirname`);
 }
