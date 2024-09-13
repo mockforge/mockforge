0c489e20e088b84eef9857858d5c9bc8d592cb9e
DiamondYuan <admin@diamondyuan.com>
2024-09-13 22:21:22 +0800

    fix: fix getdirname

--- src/extension/node/dirname.ts
+++ src/extension/node/dirname.ts
@@ -1,7 +1,10 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 export function getDirname() {
   if (typeof import.meta !== 'undefined' && import.meta.url) {
-    // const __filename = fileURLToPath(import.meta.url);
-    return new URL('.', import.meta.url).pathname;
+    const __filename = fileURLToPath(import.meta.url);
+    return dirname(__filename);
   }
   if (typeof __dirname !== 'undefined') {
     return __dirname;
