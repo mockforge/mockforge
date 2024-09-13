8c09b0b718830bdcea3d65a10c18061a9664ae4b
DiamondYuan <admin@diamondyuan.com>
2024-09-13 22:10:47 +0800

    fix: fix extesnion cannot work on windows

--- src/extension/node/dirname.ts
+++ src/extension/node/dirname.ts
@@ -1,12 +1,9 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 export function getDirname() {
   if (typeof __dirname !== 'undefined') {
     return __dirname;
   } else if (typeof import.meta !== 'undefined' && import.meta.url) {
-    const __filename = fileURLToPath(import.meta.url);
-    return (__dirname = path.dirname(__filename));
+    // const __filename = fileURLToPath(import.meta.url);
+    return new URL('.', import.meta.url).pathname;
   }
   throw new Error(`Unable to find current dirname`);
 }
