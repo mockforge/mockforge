d903aff79e11a24fa7ba107f6e8392bfa7e5a3bf
DiamondYuan <admin@diamondyuan.com>
2024-10-11 23:54:03 +0800

    feat: add e2e

---
+++ src/e2e/vite.spec.ts
@@ -0,0 +1,79 @@
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { chromium } from 'playwright';
+import { exec } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { promisify } from 'util';
+import { ChildProcess } from 'node:child_process';
+
+const execAsync = promisify(exec);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let devProcess: ChildProcess;
+let LOCAL_URL = '';
+let MOCK_FORGE_URL = '';
+
+describe('Vite Demo Tests', () => {
+  beforeEach(async () => {
+    // 1. 进入指定目录
+    const demoDir = path.resolve(__dirname, '../../demo/vite-demo');
+    process.chdir(demoDir);
+
+    // 2. 执行 pnpm i
+    try {
+      await execAsync('pnpm i');
+    } catch (error) {
+      console.error(`执行错误: ${error}`);
+      throw error;
+    }
+
+    // 3. 执行 pnpm run dev
+    devProcess = exec('pnpm run dev');
+
+    // 等待服务启动并提取 URL
+    await new Promise<void>((resolve) => {
+      devProcess.stdout!.on('data', (data) => {
+        const lines = data.toString().split('\n');
+        for (const line of lines) {
+          if (line.includes('Local:')) {
+            LOCAL_URL = line.split('Local:')[1].trim();
+          }
+          if (line.includes('[MockForge] start at')) {
+            MOCK_FORGE_URL = line.split('[MockForge] start at')[1].trim();
+          }
+        }
+
+        if (LOCAL_URL && MOCK_FORGE_URL) {
+          resolve();
+        }
+      });
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  });
+
+  it('should access MOCK_FORGE_URL and LOCAL_URL', async () => {
+    const browser = await chromium.launch({ headless: true });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    console.log('MOCK_FORGE_URL', MOCK_FORGE_URL);
+    await page.goto(MOCK_FORGE_URL);
+    await page.waitForSelector('[data-mock-state="Vite Demo"]');
+    await page.click('[data-mock-state="Vite Demo"]');
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    await page.screenshot({ path: path.resolve(__dirname, 'cache/mockforge.png') });
+    await page.goto(LOCAL_URL);
+    await page.screenshot({ path: path.resolve(__dirname, 'cache/vite-demo.png') });
+
+    expect(await page.innerHTML('body')).toMatchFileSnapshot('vite-demo-e2e.html');
+    await browser.close();
+  }, 100000);
+
+  afterEach(() => {
+    if (devProcess) {
+      devProcess.kill();
+    }
+  });
+});
