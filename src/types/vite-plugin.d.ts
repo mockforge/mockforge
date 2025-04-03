7097caa2e9556f4ff1f76c986d26335510cb9dbe
DiamondYuan <admin@diamondyuan.com>
2025-04-03 17:47:35 +0800

    fix: 升级版本

--- src/types/vite-plugin.d.ts
+++
@@ -1,7 +0,0 @@
-interface MockForgeOption {
-  mockDataDir?: string;
-  port?: number;
-  host?: string;
-}
-
-export function mockForge(option?: MockForgeOption): any;
--- src/types/webpack5-plugin.d.ts
+++
@@ -1,7 +0,0 @@
-interface MockForgeOption {
-  mockDataDir?: string;
-  port?: number;
-  host?: string;
-}
-
-export function MockForgeWebpackPlugin(option?: MockForgeOption): any;
